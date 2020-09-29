2c1f092e72dbd023f43cf60c104fbc3d18a40434
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2020-09-29 09:38:23 +0200

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -117,5 +117,25 @@ var TxtRotate = function(el, toRotate, period) {
   };
 // END Animated txt
 
+// scroll to top button
+const scrollToTop = document.getElementById('scroll-to-top');
+let dataShow = false;
 
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY > 50 && !dataShow){
+        scrollToTop.setAttribute('data-show', 'true');
+        dataShow = true;
+    }
 
+    if(window.scrollY <= 50 && dataShow){
+        scrollToTop.setAttribute('data-show', 'false');
+        dataShow = false;
+    }
+});
+
+scrollToTop.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
